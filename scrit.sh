f51c0af5c6a512c58445f0e0e02c3c113ded84a8
plimdahl <lassepost@hotmail.dk>
2023-05-30 10:08:15 +0200

    save as zip

--- scrit.sh
+++ scrit.sh
@@ -6,6 +6,7 @@ take_screenshot() {
     local filename="screenshot_${timestamp}.png"
     scrot "$filename"
     echo "Screenshot saved: $filename"
+    echo "$filename" >> screenshot_list.txt
 }
 
 # Read the duration from the user
@@ -15,9 +16,20 @@ read -p "Enter the duration in seconds: " duration
 echo "Taking screenshots for $duration seconds..."
 end_time=$((SECONDS + duration))
 
+# Create a temporary directory to store the screenshots
+temp_dir=$(mktemp -d)
+screenshot_list="$temp_dir/screenshot_list.txt"
+
 while [ $SECONDS -lt $end_time ]; do
     take_screenshot
     sleep 1
 done
 
-echo "Screenshot capture completed!"
+# Create a zip file with the screenshots
+zip_file="screenshots.zip"
+zip -r "$zip_file" "$temp_dir"/*
+
+# Clean up temporary files and directory
+rm -rf "$temp_dir"
+
+echo "Screenshots saved in $zip_file"
