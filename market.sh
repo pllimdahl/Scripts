6937d30df8191b852216d4c519c4f6bea9debfe1
plimdahl <lassepost@hotmail.dk>
2023-08-08 19:17:05 +0200

    add the rest

--- market.sh
+++ market.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo systemctl stop cinemataztic-player
+
 # Array of options
-options=("cinemataztic-en" "cinesa-es" "drf-dk" "finnkino-fi" "itv-in" "mdn-no" "valmorgan-au" "valmorgan-nz" "weischer-de" "redyplay-de" "wideeyemedia-ie")
+options=("cinemataztic-en" "cinesafun-es" "drf-dk" "finnkino-fi" "itv-in" "mdn-no" "valmorgan-au" "valmorgan-nz" "weischer-de" "redyplay-de" "wideeyemedia-ie")
 
 # Display the menu
-echo "Choose an option:"
+echo "Choose a market:"
 for ((i=0; i<${#options[@]}; i++)); do
     echo "$((i+1)). ${options[i]}"
 done
@@ -24,4 +26,16 @@ selected_option="${options[choice-1]}"
 sudo sed -i "s/^DefaultEnvironment=MARKET=.*/DefaultEnvironment=MARKET=$selected_option/" /etc/systemd/system.conf
 
 echo "MARKET environment variable updated to '$selected_option' in /etc/systemd/system.conf"
+
+sleep 3
+
+sudo rm /home/player/conf-db.json
+sudo rm -r /home/player/Assets/*
+
+read -p "Do you want to reboot the system now to apply the changes? (y/n): " reboot_choice
+if [[ $reboot_choice =~ ^[Yy]$ ]]; then
+    sudo reboot
+else
+    echo "Reboot not requested. Exiting script."
+fi
 
